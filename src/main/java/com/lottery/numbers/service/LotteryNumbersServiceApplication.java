645488bb2ca238d037d9f3e425e3e99041d1826e
Jakub Wylezek <jakub.wylezek@wp.pl>
2019-11-11 17:47:00 +0100

    Initial commit

---
+++ src/main/java/com/lottery/numbers/service/LotteryNumbersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lottery.numbers.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryNumbersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryNumbersServiceApplication.class, args);
+    }
+
+}
